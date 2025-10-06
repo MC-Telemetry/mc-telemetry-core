0a6d1220e908860f51272c35a25bbbf92cc27464
Lars Westermann <git@lars-westermann.de>
2025-10-06 20:42:22 +0200

    Add ruby block

--- common/src/main/kotlin/de/mctelemetry/core/OTelCoreMod.kt
+++ common/src/main/kotlin/de/mctelemetry/core/OTelCoreMod.kt
@@ -1,11 +1,14 @@
 package de.mctelemetry.core
 
+import dev.architectury.registry.CreativeTabRegistry
 import dev.architectury.registry.registries.DeferredRegister
 import dev.architectury.registry.registries.RegistrySupplier
 import net.minecraft.core.registries.Registries
+import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
-import net.minecraft.world.item.CreativeModeTabs
-import net.minecraft.world.item.Item
+import net.minecraft.world.item.*
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.state.BlockBehaviour
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import java.util.function.Supplier
@@ -17,19 +20,42 @@ object OTelCoreMod {
 
     val logger: Logger = LogManager.getLogger(MOD_ID)
 
+    val TABS: DeferredRegister<CreativeModeTab> = DeferredRegister.create(MOD_ID, Registries.CREATIVE_MODE_TAB)
+    val BLOCKS: DeferredRegister<Block> = DeferredRegister.create(MOD_ID, Registries.BLOCK)
     val ITEMS: DeferredRegister<Item> = DeferredRegister.create(MOD_ID, Registries.ITEM);
 
-    var RUBY: RegistrySupplier<Item>? = null
+    val OTEL_TAB: RegistrySupplier<CreativeModeTab>
+
+    val RUBY_BLOCK: RegistrySupplier<Block> = registerBlock("ruby_block", {
+        Block(BlockBehaviour.Properties.of())
+    })
+    val RUBY: RegistrySupplier<Item> = registerItem("ruby_block", {
+        BlockItem(RUBY_BLOCK.get(), Item.Properties().`arch$tab`(OTEL_TAB))
+    })
+
+    init {
+        OTEL_TAB = TABS.register(
+            "mcotelcore_tab",
+            {
+                CreativeTabRegistry.create(
+                    Component.translatable("category.mcotelcore_tab"),
+                    Supplier { ItemStack(RUBY) }
+                )
+            }
+        )
+    }
 
     fun init() {
-        RUBY = registerItem("ruby", {
-            Item(Item.Properties().`arch$tab`(CreativeModeTabs.INGREDIENTS))
-        })
-
+        TABS.register()
+        BLOCKS.register()
         ITEMS.register()
     }
 
     fun registerItem(name: String, item: Supplier<Item>): RegistrySupplier<Item> {
         return ITEMS.register(ResourceLocation.fromNamespaceAndPath(MOD_ID, name), item);
+    }
+
+    fun registerBlock(name: String, block: Supplier<Block?>?): RegistrySupplier<Block> {
+        return BLOCKS.register(ResourceLocation.fromNamespaceAndPath(MOD_ID, name), block)
     }
 }
