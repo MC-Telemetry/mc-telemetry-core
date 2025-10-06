2a16bbc9f6202fcfcff83e00d9cbb985ff672eaa
Lars Westermann <git@lars-westermann.de>
2025-10-06 20:13:26 +0200

    Add ruby item

--- common/src/main/kotlin/de/mctelemetry/core/OTelCoreMod.kt
+++ common/src/main/kotlin/de/mctelemetry/core/OTelCoreMod.kt
@@ -1,7 +1,15 @@
 package de.mctelemetry.core
 
+import dev.architectury.registry.registries.DeferredRegister
+import dev.architectury.registry.registries.RegistrySupplier
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.CreativeModeTabs
+import net.minecraft.world.item.Item
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import java.util.function.Supplier
+
 
 object OTelCoreMod {
 
@@ -9,6 +17,19 @@ object OTelCoreMod {
 
     val logger: Logger = LogManager.getLogger(MOD_ID)
 
+    val ITEMS: DeferredRegister<Item> = DeferredRegister.create(MOD_ID, Registries.ITEM);
+
+    var RUBY: RegistrySupplier<Item>? = null
+
     fun init() {
+        RUBY = registerItem("ruby", {
+            Item(Item.Properties().`arch$tab`(CreativeModeTabs.INGREDIENTS))
+        })
+
+        ITEMS.register()
+    }
+
+    fun registerItem(name: String, item: Supplier<Item>): RegistrySupplier<Item> {
+        return ITEMS.register(ResourceLocation.fromNamespaceAndPath(MOD_ID, name), item);
     }
 }
