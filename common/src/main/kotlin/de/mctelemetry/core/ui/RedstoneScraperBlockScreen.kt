17265c69e487c26c53cecc996bf04cf9b12cf4a1
Lars Westermann <git@lars-westermann.de>
2025-10-15 19:50:03 +0200

    Add first working scroll widget

--- common/src/main/kotlin/de/mctelemetry/core/ui/RedstoneScraperBlockScreen.kt
+++ common/src/main/kotlin/de/mctelemetry/core/ui/RedstoneScraperBlockScreen.kt
@@ -6,7 +6,12 @@ import de.mctelemetry.core.utils.dsl.components.IComponentDSLBuilder.Companion.b
 import de.mctelemetry.core.utils.dsl.components.append
 import de.mctelemetry.core.utils.dsl.components.style
 import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.client.gui.components.AbstractScrollWidget
+import net.minecraft.client.gui.components.AbstractWidget
 import net.minecraft.client.gui.components.EditBox
+import net.minecraft.client.gui.components.Renderable
+import net.minecraft.client.gui.components.events.GuiEventListener
+import net.minecraft.client.gui.narration.NarrationElementOutput
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
 import net.minecraft.client.renderer.GameRenderer
 import net.minecraft.network.chat.Component
@@ -26,8 +31,14 @@ class RedstoneScraperBlockScreen(menu: RedstoneScraperBlockMenu, playerInventory
 
         super.init()
 
-        val editBox = EditBox(font, leftPos + 10, topPos + 20, 150, 20, buildComponent { +"EditBox" })
-        addRenderableWidget(editBox)
+        val scrollWidget = ScrollWidget(leftPos + 10, topPos + 10, 150, 150, buildComponent { +"Test" })
+        val rowWidget1 = RowWidget(10, 10, 130, 20, buildComponent { +"Test" })
+        val rowWidget2 = RowWidget(10, 30, 130, 20, buildComponent { +"Test" })
+
+        scrollWidget.addChildren(rowWidget1)
+        scrollWidget.addChildren(rowWidget2)
+
+        addRenderableWidget(scrollWidget)
     }
 
     override fun renderBg(guiGraphics: GuiGraphics, partialTick: Float, mouseX: Int, mouseY: Int) {
@@ -43,14 +54,14 @@ class RedstoneScraperBlockScreen(menu: RedstoneScraperBlockMenu, playerInventory
     override fun render(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
         super.render(guiGraphics, mouseX, mouseY, partialTick)
 
-        guiGraphics.drawString(font, buildComponent {
-            +"Redstone: "
-            append(menu.data.get(0).toString()) {
-                style {
-                    isBold = true
-                }
-            }
-        }, leftPos + 10, topPos + 50, 0x909090, false)
+//        guiGraphics.drawString(font, buildComponent {
+//            +"Redstone: "
+//            append(menu.data.get(0).toString()) {
+//                style {
+//                    isBold = true
+//                }
+//            }
+//        }, leftPos + 10, topPos + 50, 0x909090, false)
 
         renderTooltip(guiGraphics, mouseX, mouseY)
     }
@@ -61,5 +72,64 @@ class RedstoneScraperBlockScreen(menu: RedstoneScraperBlockMenu, playerInventory
                 OTelCoreMod.MOD_ID,
                 "textures/gui/redstone_scraper_block/redstone_scraper_block.png"
             )
+    }
+
+    private class ScrollWidget(leftPos: Int, topPos: Int, width: Int, height: Int, component: Component) :
+        AbstractScrollWidget(leftPos, topPos, width, height, component) {
+
+        private val children: MutableList<RowWidget> = mutableListOf()
+
+        override fun getInnerHeight(): Int {
+            return 1000
+        }
+
+        override fun scrollRate(): Double {
+            return 1.0
+        }
+
+        fun addChildren(rowWidget: RowWidget) {
+            children.add(rowWidget)
+        }
+
+        override fun renderContents(
+            guiGraphics: GuiGraphics,
+            i: Int,
+            j: Int,
+            f: Float
+        ) {
+            val pos = guiGraphics.pose()
+            pos.pushPose()
+            pos.translate(x.toDouble(), y.toDouble(), 0.0)
+
+            children.forEach { it.render(guiGraphics, i, j, f) }
+
+            pos.popPose()
+        }
+
+        override fun updateWidgetNarration(narrationElementOutput: NarrationElementOutput) {
+        }
+    }
+
+    private inner class RowWidget(leftPos: Int, topPos: Int, width: Int, height: Int, component: Component) :
+        AbstractWidget(leftPos, topPos, width, height, component) {
+
+        override fun renderWidget(
+            guiGraphics: GuiGraphics,
+            i: Int,
+            j: Int,
+            f: Float
+        ) {
+            guiGraphics.drawString(font, buildComponent {
+                +"Redstone: "
+                append(menu.data.get(0).toString()) {
+                    style {
+                        isBold = true
+                    }
+                }
+            }, x, y, 0x909090, false)
+        }
+
+        override fun updateWidgetNarration(narrationElementOutput: NarrationElementOutput) {
+        }
     }
 }
