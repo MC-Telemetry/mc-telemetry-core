ccbaf1570b1b85283f51f2810e0c9d2617785518
Alexander Schwerin <alexander_C._schwerin@web.de>
2025-10-25 04:45:31 +0200

    Add ObsSrcContainerBlockEntity serialization

--- common/src/main/kotlin/de/mctelemetry/core/blocks/entities/OTelCoreModBlockEntityTypes.kt
+++ common/src/main/kotlin/de/mctelemetry/core/blocks/entities/OTelCoreModBlockEntityTypes.kt
@@ -1,6 +1,7 @@
 package de.mctelemetry.core.blocks.entities
 
 import de.mctelemetry.core.OTelCoreMod
+import de.mctelemetry.core.blocks.observation.ObservationSourceContainerBlockEntity
 import dev.architectury.registry.registries.DeferredRegister
 import dev.architectury.registry.registries.RegistrySupplier
 import net.minecraft.core.registries.Registries
@@ -9,18 +10,20 @@ import net.minecraft.world.level.block.entity.BlockEntityType
 import java.util.function.Supplier
 
 object OTelCoreModBlockEntityTypes {
+
     private val BLOCK_ENTITIES
             : DeferredRegister<BlockEntityType<*>> =
         DeferredRegister.create<BlockEntityType<*>>(OTelCoreMod.MOD_ID, Registries.BLOCK_ENTITY_TYPE)
 
     lateinit var REDSTONE_SCRAPER_BLOCK_ENTITY: RegistrySupplier<BlockEntityType<RedstoneScraperBlockEntity>>
+    lateinit var OBSERVATION_SOURCE_CONTAINER_BLOCK_ENTITY: RegistrySupplier<BlockEntityType<ObservationSourceContainerBlockEntity>>
 
     fun writeRegister() {
         BLOCK_ENTITIES.register()
     }
 
     fun <T : BlockEntityType<*>> registerBlockEntity(
-        name: String, blockEntity: Supplier<T>
+        name: String, blockEntity: Supplier<T>,
     ): RegistrySupplier<T> {
         return BLOCK_ENTITIES.register<T>(ResourceLocation.fromNamespaceAndPath(OTelCoreMod.MOD_ID, name), blockEntity)
     }
