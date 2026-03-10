1371f58902c67f2acb4a7705cd5a815e330e3ba1
Alexander Schwerin <8933715+Zincfox@users.noreply.github.com>
2026-03-10 17:01:36 +0100

    Implement ScraperCoreRenderLayer
    Also rework ScraperStatusRenderLayer to use DefaultedGeoModel instead of passing ResourceLocations around explicitly.

--- common/src/main/kotlin/de/mctelemetry/core/geo/renderer/blocks/entities/layers/ScraperCoreRenderLayer.kt
+++ common/src/main/kotlin/de/mctelemetry/core/geo/renderer/blocks/entities/layers/ScraperCoreRenderLayer.kt
@@ -1,9 +1,114 @@
 package de.mctelemetry.core.geo.renderer.blocks.entities.layers
 
+import com.mojang.blaze3d.vertex.PoseStack
+import com.mojang.blaze3d.vertex.VertexConsumer
+import de.mctelemetry.core.OTelCoreMod
 import de.mctelemetry.core.blocks.entities.ScraperBlockEntity
+import net.minecraft.client.renderer.MultiBufferSource
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.client.renderer.texture.MissingTextureAtlasSprite
+import net.minecraft.core.Holder
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.level.block.Block
+import software.bernie.geckolib.cache.`object`.BakedGeoModel
+import software.bernie.geckolib.model.DefaultedGeoModel
 import software.bernie.geckolib.renderer.GeoRenderer
 import software.bernie.geckolib.renderer.layer.GeoRenderLayer
+import kotlin.jvm.optionals.getOrElse
 
 class ScraperCoreRenderLayer(entityRendererIn: GeoRenderer<ScraperBlockEntity>) :
     GeoRenderLayer<ScraperBlockEntity>(entityRendererIn) {
+
+    object CoreModel : DefaultedGeoModel<ScraperBlockEntity>(
+        ResourceLocation.fromNamespaceAndPath(
+            OTelCoreMod.MOD_ID,
+            "default"
+        )
+    ) {
+        override fun subtype(): String = "mcotelcore/scraper_core"
+
+        private fun getBlockResourceLocation(animatable: ScraperBlockEntity): ResourceLocation? {
+            val holder = animatable.blockState.blockHolder
+            return holder.unwrapKey().getOrElse {
+                logMissingBlockKeyWarning(holder)
+                return null
+            }.location()
+        }
+
+        private fun getCoreTextureForBlockResource(blockResourceLocation: ResourceLocation): ResourceLocation {
+            return blockResourceLocation.withPath("textures/mcotelcore/scraper_core/${blockResourceLocation.path}.png")
+        }
+
+        private fun getCoreAnimationForBlockResource(blockResourceLocation: ResourceLocation): ResourceLocation {
+            return blockResourceLocation.withPath("animations/mcotelcore/scraper_core/${blockResourceLocation.path}.animation.json")
+        }
+
+        @Deprecated("Deprecated")
+        override fun getTextureResource(animatable: ScraperBlockEntity): ResourceLocation {
+            return getCoreTextureForBlockResource(
+                getBlockResourceLocation(animatable)
+                    ?: return MissingTextureAtlasSprite.getLocation()
+            )
+        }
+
+        override fun getAnimationResource(animatable: ScraperBlockEntity): ResourceLocation {
+            return getCoreAnimationForBlockResource(
+                getBlockResourceLocation(animatable)
+                    ?: return MissingTextureAtlasSprite.getLocation().withSuffix(".animation.json")
+            )
+        }
+    }
+
+    companion object {
+
+        private val missingBlockWarningsLogged: MutableSet<Block> = mutableSetOf()
+        private fun logMissingBlockKeyWarning(holder: Holder<Block>) {
+            val block = holder.value()!!
+            if (missingBlockWarningsLogged.add(block)) {
+                OTelCoreMod.logger.warn("Could not find id of block {}", block)
+            }
+        }
+
+    }
+
+    override fun render(
+        poseStack: PoseStack,
+        animatable: ScraperBlockEntity,
+        bakedModel: BakedGeoModel,
+        renderType: RenderType?,
+        bufferSource: MultiBufferSource,
+        buffer: VertexConsumer?,
+        partialTick: Float,
+        packedLight: Int,
+        packedOverlay: Int
+    ) {
+        super.render(
+            poseStack,
+            animatable,
+            bakedModel,
+            renderType,
+            bufferSource,
+            buffer,
+            partialTick,
+            packedLight,
+            packedOverlay
+        )
+        if (buffer == null) return
+        val renderer = renderer
+        val coreModel = CoreModel.getBakedModel(CoreModel.getModelResource(animatable))
+        val coreTexture = CoreModel.getTextureResource(animatable, renderer)
+        val coreRenderType = CoreModel.getRenderType(animatable, coreTexture) ?: return
+        renderer.reRender(
+            coreModel,
+            poseStack,
+            bufferSource,
+            animatable,
+            coreRenderType,
+            if (coreRenderType === renderType) buffer else bufferSource.getBuffer(coreRenderType),
+            partialTick,
+            packedLight,
+            packedOverlay,
+            renderer.getRenderColor(animatable, partialTick, packedLight).argbInt,
+        )
+    }
 }
--- common/src/main/kotlin/de/mctelemetry/core/geo/renderer/blocks/entities/layers/ScraperStatusRenderLayer.kt
+++ common/src/main/kotlin/de/mctelemetry/core/geo/renderer/blocks/entities/layers/ScraperStatusRenderLayer.kt
@@ -11,30 +11,64 @@ import net.minecraft.client.renderer.RenderType
 import net.minecraft.resources.ResourceLocation
 import software.bernie.geckolib.cache.`object`.BakedGeoModel
 import software.bernie.geckolib.cache.texture.AutoGlowingTexture
-import software.bernie.geckolib.model.DefaultedBlockGeoModel
-import software.bernie.geckolib.model.GeoModel
+import software.bernie.geckolib.model.DefaultedGeoModel
 import software.bernie.geckolib.renderer.GeoRenderer
 import software.bernie.geckolib.renderer.layer.GeoRenderLayer
 
 class ScraperStatusRenderLayer(entityRendererIn: GeoRenderer<ScraperBlockEntity>) :
     GeoRenderLayer<ScraperBlockEntity>(entityRendererIn) {
 
-    companion object {
-        val StatusModelLocation: ResourceLocation =
-            ResourceLocation.fromNamespaceAndPath(OTelCoreMod.MOD_ID, "geo/block/scraper_error.geo.json")
+    object StatusModel : DefaultedGeoModel<ScraperBlockEntity>(
+        ResourceLocation.fromNamespaceAndPath(OTelCoreMod.MOD_ID, "default")
+    ) {
+        override fun subtype(): String = "mcotelcore/scraper_status"
+
         val ErrorTextureLocation: ResourceLocation =
-            ResourceLocation.fromNamespaceAndPath(OTelCoreMod.MOD_ID, "textures/scraper_status/scraper_error.png")
+            ResourceLocation.fromNamespaceAndPath(
+                OTelCoreMod.MOD_ID,
+                "textures/mcotelcore/scraper_status/scraper_error.png"
+            )
         val OkTextureLocation: ResourceLocation =
-            ResourceLocation.fromNamespaceAndPath(OTelCoreMod.MOD_ID, "textures/scraper_status/scraper_ok.png")
+            ResourceLocation.fromNamespaceAndPath(
+                OTelCoreMod.MOD_ID,
+                "textures/mcotelcore/scraper_status/scraper_ok.png"
+            )
         val WarningTextureLocation: ResourceLocation =
-            ResourceLocation.fromNamespaceAndPath(OTelCoreMod.MOD_ID, "textures/scraper_status/scraper_warning.png")
+            ResourceLocation.fromNamespaceAndPath(
+                OTelCoreMod.MOD_ID,
+                "textures/mcotelcore/scraper_status/scraper_warning.png"
+            )
         val NotConfiguredTextureLocation: ResourceLocation = ResourceLocation.fromNamespaceAndPath(
             OTelCoreMod.MOD_ID,
-            "textures/scraper_status/scraper_not_configured.png"
+            "textures/mcotelcore/scraper_status/scraper_not_configured.png"
+        )
+        val ErrorAnimationLocation: ResourceLocation =
+            ResourceLocation.fromNamespaceAndPath(
+                OTelCoreMod.MOD_ID,
+                "animations/mcotelcore/scraper_status/scraper_error.png"
+            )
+        val OkAnimationLocation: ResourceLocation =
+            ResourceLocation.fromNamespaceAndPath(
+                OTelCoreMod.MOD_ID,
+                "animations/mcotelcore/scraper_status/scraper_ok.png"
+            )
+        val WarningAnimationLocation: ResourceLocation =
+            ResourceLocation.fromNamespaceAndPath(
+                OTelCoreMod.MOD_ID,
+                "animations/mcotelcore/scraper_status/scraper_warning.png"
+            )
+        val NotConfiguredAnimationLocation: ResourceLocation = ResourceLocation.fromNamespaceAndPath(
+            OTelCoreMod.MOD_ID,
+            "animations/mcotelcore/scraper_status/scraper_not_configured.png"
         )
 
-        fun getStatusTexture(errorState: ObservationSourceErrorState.Type): ResourceLocation {
-            return when (errorState) {
+
+        private fun getStatus(animatable: ScraperBlockEntity): ObservationSourceErrorState.Type {
+            return animatable.blockState.getValue(ObservationSourceContainerBlock.ERROR)
+        }
+
+        private fun getStatusTextureForStateType(type: ObservationSourceErrorState.Type): ResourceLocation {
+            return when (type) {
                 ObservationSourceErrorState.Type.NotConfigured -> NotConfiguredTextureLocation
                 ObservationSourceErrorState.Type.Ok -> OkTextureLocation
                 ObservationSourceErrorState.Type.Warnings -> WarningTextureLocation
@@ -42,7 +76,31 @@ class ScraperStatusRenderLayer(entityRendererIn: GeoRenderer<ScraperBlockEntity>
             }
         }
 
-        val STATUS_MODEL: GeoModel<ScraperBlockEntity> = DefaultedBlockGeoModel(StatusModelLocation)
+        private fun getStatusAnimationForStateType(type: ObservationSourceErrorState.Type): ResourceLocation {
+            return when (type) {
+                ObservationSourceErrorState.Type.NotConfigured -> NotConfiguredAnimationLocation
+                ObservationSourceErrorState.Type.Ok -> OkAnimationLocation
+                ObservationSourceErrorState.Type.Warnings -> WarningAnimationLocation
+                ObservationSourceErrorState.Type.Errors -> ErrorAnimationLocation
+            }
+        }
+
+        @Deprecated("Deprecated")
+        override fun getTextureResource(animatable: ScraperBlockEntity): ResourceLocation {
+            return getStatusTextureForStateType(
+                getStatus(animatable)
+            )
+        }
+
+        override fun getAnimationResource(animatable: ScraperBlockEntity): ResourceLocation {
+            return getStatusAnimationForStateType(
+                getStatus(animatable)
+            )
+        }
+
+        override fun getRenderType(animatable: ScraperBlockEntity, texture: ResourceLocation): RenderType {
+            return AutoGlowingTexture.getRenderType(texture)
+        }
     }
 
     override fun render(
@@ -69,9 +127,9 @@ class ScraperStatusRenderLayer(entityRendererIn: GeoRenderer<ScraperBlockEntity>
         )
         if (buffer == null) return
         val renderer = renderer
-        val statusModel = STATUS_MODEL.getBakedModel(StatusModelLocation)
-        val errorState = animatable.blockState.getValue(ObservationSourceContainerBlock.ERROR)
-        val statusRenderType = AutoGlowingTexture.getRenderType(getStatusTexture(errorState))
+        val statusModel = StatusModel.getBakedModel(StatusModel.getModelResource(animatable, renderer))
+        val statusTexture = StatusModel.getTextureResource(animatable, renderer)
+        val statusRenderType = StatusModel.getRenderType(animatable, statusTexture)
         renderer.reRender(
             statusModel,
             poseStack,
