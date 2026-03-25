6eb8b3149d16d51e6bda56b09586e7b357d18a52
Lars Westermann <git@lars-westermann.de>
2026-03-25 19:21:08 +0100

    Fix command name

--- common/src/main/kotlin/de/mctelemetry/core/commands/metrics/CommandMetricsCreate.kt
+++ common/src/main/kotlin/de/mctelemetry/core/commands/metrics/CommandMetricsCreate.kt
@@ -24,7 +24,7 @@ class CommandMetricsCreate internal constructor(
     private val buildContext: CommandBuildContext,
 ) {
 
-    val command = buildCommand("data/create") {
+    val command = buildCommand("create") {
         requires { it.hasPermission(2) }
         "gauge" {
             argument("type", EnumArgumentType(InstrumentExportType::class.java)) {
